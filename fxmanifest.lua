56dd8cc8725dd17ec3442c67c01501e65cdc88d2
Flitcher <contact@bpoisson.com>
2026-02-22 16:12:07 +0000

    FIX dependencie missing

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,6 +5,10 @@ author 'Flitcher'
 description 'Copy vector3 from camera position'
 version '1.0.0'
 
+dependencies {
+	'ox_lib',
+}
+
 shared_script '@ox_lib/init.lua'
 
 client_script 'client/client.lua'
